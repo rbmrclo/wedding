54f9c8af6baf7724258e3580301877a18db9575a
Robbie Marcelo <rbmrclo@hotmail.com>
2017-12-26 23:49:59 +0800

    Setup ecto and node mods

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,6 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :wedding, Wedding.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "wedding_dev",
   hostname: "localhost",
   pool_size: 10
