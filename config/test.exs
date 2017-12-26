9d34acb7b5b5653a4096dd35ba4d248e9c99dd95
Robbie Marcelo <rbmrclo@hotmail.com>
2017-12-27 00:22:18 +0800

    Configure test.exs

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,6 @@ config :logger, level: :warn
 # Configure your database
 config :wedding, Wedding.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "wedding_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
