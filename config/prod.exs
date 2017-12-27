8f564eab98d93a0a8890a9263e3e1ff5e188b632
Robbie Marcelo <rbmrclo@hotmail.com>
2017-12-27 15:02:53 +0800

    Fix config

--- config/prod.exs
+++ config/prod.exs
@@ -15,9 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :wedding, WeddingWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: System.get_env("HOST_NAME"), port: 443],
-
-force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [scheme: "http", host: System.get_env("HOST_NAME"), port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
